73cb4cc947b47c8de0b5245803e00a7e5ae23cd3
magdy shata <magdyshata35@gmail.com>
2025-01-04 13:03:25 +0200

    commit-4

--- app/app.component.ts
+++ app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent {
       'animation'
     ];
     console.log("gsgagtr")
-    console.log("gsgagtrffffffffffffffffffffffff")
+    console.log("gsgagtrffffffffffffffffffffffffhhhhhhhhhhhhhhh")
 
   }
 
