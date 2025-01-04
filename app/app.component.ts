2cb7c104f353047f6050fac4082f3033e5c28928
magdy shata <magdyshata35@gmail.com>
2025-01-04 18:07:03 +0200

    min2-1

--- app/app.component.ts
+++ app/app.component.ts
@@ -38,6 +38,7 @@ export class AppComponent {
     console.log("gsgagtr1111111111111111111")
     console.log("gsgagtr1111111111111111111")
     console.log("gsgagtr1111111111111111111000000000000000000")
+    console.log("gsgagtr1111111111111111111000000000000000000")
 
 
   }
